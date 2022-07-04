8d82c40d7d08707dee8707e220b57b31e6a5c959
nsantos16 <nicolassantos16@gmail.com>
2022-07-04 12:43:26 -0300

    Change selector on EmojiSearch

--- src/components/emoji-picker/EmojiSearch/index.tsx
+++ src/components/emoji-picker/EmojiSearch/index.tsx
@@ -1,14 +1,13 @@
 import { SearchIcon } from '@heroicons/react/outline';
 import { useState } from 'react';
 import { onSearch as onSearchAction } from '../../../state/slices/emojiSlice';
-import { useAppDispatch } from '../../../state/hooks';
+import { useAppDispatch, useAppSelector } from '../../../state/hooks';
 
 const EmojiSearch = () => {
   const dispatch = useAppDispatch();
-  const [search, setSearch] = useState('');
+  const searchKeyword = useAppSelector((state) => state.emoji.searchFilter);
 
   const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
     dispatch(onSearchAction(e.target.value));
   };
 
@@ -16,7 +15,7 @@ const EmojiSearch = () => {
     <div className="relative w-full flex items-center">
       <SearchIcon className="w-4 h-4 ml-2 absolute pointer-events-none" />
       <input
-        value={search}
+        value={searchKeyword}
         onChange={onSearch}
         className="rounded pl-7 border border-gray-200 px-2 py-1 w-full text-sm"
       />
